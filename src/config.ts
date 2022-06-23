27b611fd36bc403bdbd78b5d1dd7c27db12f0c2e
derek <hiddenderek@gmail.com>
2022-06-23 08:03:03 -0500

    changed host name to domain name

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ const config = {
     port: typeof process != "undefined" ? (process?.env?.PORT || 8100) : 8100,
     authPort: typeof process != "undefined" ? (process?.env?.PORT || 8090) : 8090,
     httpPort: 8080,
-    hostname: "159.223.199.71"
+    hostname: "dchapman-portfolio.site"
 }
 export default config
