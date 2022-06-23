f9984a09f2b4aa8d2eae3163a379721ee2f891a1
hiddenderek <hiddenderek@gmail.com>
2022-06-22 21:17:59 -0500

    tweaked hostname functionality

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ const config = {
     port: typeof process != "undefined" ? (process?.env?.PORT || 8100) : 8100,
     authPort: typeof process != "undefined" ? (process?.env?.PORT || 8090) : 8090,
     httpPort: 8080,
-    hostname: "localhost"
+    hostname: typeof window != "undefined" ? window.location.hostname : "localhost"
 }
 export default config
